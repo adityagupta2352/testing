1df6f8e04cadf4fe0c932c6c73533f23498c6f4a
Aditya Gupta <aditya.gupta@innogent.in>
2023-12-26 12:57:13 +0530

    First Commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+k
+;github.example/src/main/java/com/github/example/GitHub.java,d\6\d643896910c0e7cb2b8517f726a515920f82a360
---
+++ github.example/src/main/java/com/github/example/GitHub.java
@@ -0,0 +1,20 @@
+package com.github.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class GitHub {
+
+    private Long id;
+
+    private String name;
+
+    @GetMapping("/")
+    public String sayHello(){
+        System.out.println("Hello Dunia");
+        return "Hello";
+    }
+};
